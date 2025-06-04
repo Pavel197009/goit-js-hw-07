3d575e4a24e851f6404f32e02ea35bfcd7649fe2
Pavel197009 <parutman@gmail.com>
2025-06-04 09:36:32 +0300

    Add files via upload

--- js/task-1.js
+++ js/task-1.js
@@ -1,8 +1,8 @@
-const item = document.querySelectorAll(".item ul");
+const item = document.querySelectorAll(".item");
 const h2Categories = document.querySelectorAll("h2");
 console.log(`Number of categories: ${item.length}`);
-for (let i = 0; i < item.length; i++) {
-    let count = item[i].children.length;
+for (let i = 0; i < h2Categories.length; i++) {
+    let count = h2Categories[i].nextSibling.children.length;
     console.log(`Category: ${h2Categories[i].textContent}`);
     console.log(`Elements: ${count}`);
 }
