5e7610b8b8294c847c0cadbc648fe4c2df53435b
Pavel197009 <parutman@gmail.com>
2025-06-18 15:29:24 +0300

    Add files via upload

--- js/task-1.js
+++ js/task-1.js
@@ -1,8 +1,8 @@
-const item = document.querySelectorAll(".item");
-const h2Categories = document.querySelectorAll("h2");
-console.log(`Number of categories: ${item.length}`);
-for (let i = 0; i < h2Categories.length; i++) {
-    let count = h2Categories[i].nextElementSibling.children.length;
-    console.log(`Category: ${h2Categories[i].textContent}`);
-    console.log(`Elements: ${count}`);
+const items = document.querySelectorAll(".item");                               // выбор всех элементов с классом item
+const h2Categories = document.querySelectorAll("h2");                           // выбор всех элементов h2    
+console.log(`Number of categories: ${items.length}`);                           // вывод количества категорий       
+for (let i = 0; i < items.length; i++) {                                        
+    const getByTag = items.find(item => items[i].children.tagName === "UL");    // поиск потомка с тегом UL внутри каждого элемента класса item
+    console.log(`Category: ${h2Categories[i].textContent}`);                    // вывод названия категории
+    console.log(`Elements: ${getByTag.children.length}`);                       // вывод количества элементов LI в элементе UL  
 }
