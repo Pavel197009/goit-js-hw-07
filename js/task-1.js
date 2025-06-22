7c752d24f88ab25390b17b2c3e984166bc1996eb
Pavel197009 <parutman@gmail.com>
2025-06-22 21:32:32 +0300

    Add files via upload

--- js/task-1.js
+++ js/task-1.js
@@ -2,7 +2,14 @@ const items = document.querySelectorAll(".item");                               
 const h2Categories = document.querySelectorAll("h2");                           // выбор всех элементов h2    
 console.log(`Number of categories: ${items.length}`);                           // вывод количества категорий       
 for (let i = 0; i < items.length; i++) {                                        
-    const getByTag = items.find(item => items[i].children.tagName === "UL");    // поиск потомка с тегом UL внутри каждого элемента класса item
+    const childElements = items[i].children;                                    // выбор всех потомков i - го элемента с классом item
+    let ptr = 0;
+    for (let j = 0; j < childElements.length; j++) {                            // перебор с поиском элемента с тегом UL
+        if (childElements[j].tagName == "UL") {
+            ptr = j;
+            break;
+        }
+    }
     console.log(`Category: ${h2Categories[i].textContent}`);                    // вывод названия категории
-    console.log(`Elements: ${getByTag.children.length}`);                       // вывод количества элементов LI в элементе UL  
+    console.log(`Elements: ${childElements[ptr].children.length}`);             // вывод количества элементов LI в элементе UL  
 }
